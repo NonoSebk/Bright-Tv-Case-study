76deadbb4e313928335c42880739a597349073b9
lovedsebk@gmail.com <lovedsebk@gmail.com>
2026-07-26 15:52:58 +0000

    Add Bright TV analysis project files: data processing, planning, and presentation materials

---
+++ Project Planning/New Notebook 2026-07-26 15:56:08.sql
@@ -0,0 +1,3 @@
+-- Databricks notebook source
+Canva Link: https://canva.link/g5h08ex0mf0dj0y
+Miro Link: https://miro.com/app/board/uXjVHE4Vv9Q=/?share_link_id=204802223152
---
+++ Project Presentation/New Notebook 2026-07-26 16:06:22.sql
@@ -0,0 +1,22 @@
+-- Databricks notebook source
+
+Power BI Link: https://datastudio.google.com/s/pvamjXZUSGQ
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+
+-- COMMAND ----------
+
+Power BI Link: https://datastudio.google.com/s/pvamjXZUSGQ
